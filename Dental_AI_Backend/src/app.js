99a910fec844a845bbe456a1f22c259a861a293a
Jeremya <jeremiatampubolon025@gmail.com>
2026-02-23 09:26:49 +0700

    delete duplicate code

--- Dental_AI_Backend/src/app.js
+++ Dental_AI_Backend/src/app.js
@@ -30,8 +30,3 @@ if(process.env.NODE_ENV !== 'test') {
         console.log(`Server is running on port ${PORT}`);
     });
 }
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
