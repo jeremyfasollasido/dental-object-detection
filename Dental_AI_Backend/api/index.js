0e4df2427afccbf966bd79eb7ef81062cb41958d
Jeremya <jeremiatampubolon025@gmail.com>
2026-02-24 08:50:26 +0700

    again and again vercell config

--- Dental_AI_Backend/api/index.js
+++ Dental_AI_Backend/api/index.js
@@ -1,3 +1,4 @@
-const app = require('.src/app');                                                                                                                                                            
+const serverless = require('serverless-http');
+const app = require('../src/app');
 
-module.exports = app;
+module.exports = serverless(app);
