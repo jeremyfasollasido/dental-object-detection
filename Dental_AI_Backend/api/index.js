8b5ca0b09da0c7fb95c8d47940c018ba6e0c3fd7
Jeremya <jeremiatampubolon025@gmail.com>
2026-02-25 21:17:08 +0700

    Change to ES modules for Vercel compatibility

--- Dental_AI_Backend/api/index.js
+++ Dental_AI_Backend/api/index.js
@@ -1,3 +1,4 @@
-module.exports = (req, res) => {
+export default (req, res) => {
+  console.log('Function called with path:', req.url);
   res.status(200).json({ message: 'Hello from Vercel!' });
 };  
